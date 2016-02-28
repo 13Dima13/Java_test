e5e597a241bbe95c26aa452d53c8aca25e6daae8
13Dima13 <sda.freeman@gmail.com>
2016-02-28 22:03:48 +0200

    Added new classes

---
+++ sandbox/src/main/java/ua/stqa/test/sandbox/Rectangle.java
@@ -0,0 +1,15 @@
+package ua.stqa.test.sandbox;
+
+/**
+ * Created by sda on 28.02.2016.
+ */
+public class Rectangle {
+
+  public double a ;
+  public double b ;
+
+  public Rectangle (double a, double b) {
+    this.a=a;
+    this.b=b;
+  }
+}
---
+++ sandbox/src/main/java/ua/stqa/test/sandbox/Square.java
@@ -0,0 +1,13 @@
+package ua.stqa.test.sandbox;
+
+/**
+ * Created by sda on 28.02.2016.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+}
