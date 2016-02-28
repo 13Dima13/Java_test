a49ae3397f8d49e80b0d3515b022a4f2db0c0d95
13Dima13 <sda.freeman@gmail.com>
2016-02-28 23:06:56 +0200

    Update

--- sandbox/src/main/java/Homework2/Run.java
+++ sandbox/src/main/java/Homework2/Run.java
@@ -2,16 +2,16 @@ package Homework2;
 
 public class Run {
   public static void main(String[] args) {
-    Point p1 = new Point(1, 22);
-    Point p2 = new Point(3, 15);
+    Point p1 = new Point(1, 2);
+    Point p2 = new Point(3, 1);
 
     System.out.println("Distance between p2 and p1: p1(" + p1.x + "," + p1.y + ") and p2(" + p2.x + "," +
             p2.y + ") = " + distance(p1, p2));
   }
 
     public static double distance (Point p1, Point p2){
-      double dx = p2.x - p1.x;
-      double dy = p2.y - p1.y;
-      return Math.sqrt(dx * dx + dy * dy);
+      double x = p2.x - p1.x;
+      double y = p2.y - p1.y;
+      return Math.sqrt(x * x + y * y);
     }
   }
