cc78d9ad183dbe39362764ed0f7f4575e9b50a39
13Dima13 <sda.freeman@gmail.com>
2016-03-04 23:33:29 +0200

    Optimization

--- addressbook-web-tests/src/test/java/ua/stqa/test/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ua/stqa/test/addressbook/GroupCreationTests.java
@@ -1,15 +1,9 @@
 package ua.stqa.test.addressbook;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
+
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
-
-public class GroupCreationTests extends TestBase{
+public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() {
